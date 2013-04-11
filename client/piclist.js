93eaac48d6ff8e44409b89a9ce3a498c9fee7c50
lb <lb50766352>
2013-04-11 20:00:49 +0800

    init

---
+++ client/piclist.js
@@ -0,0 +1,18 @@
+var a = '1';
+console.log(1111);
+console.log(1111);
+console.log(1111);
+
+
+console.log(1111);
+console.log(1111);
+console.log(1111);
+console.log(1111);
+console.log(1111);
+console.log(1111);
+console.log(1111);
+console.log(1111);
+console.log(1111);
+console.log(1111);
+
+console.log(1111);
